8ca6306d7c5b5bd7d83c04cc595c593ad608f4d4
Pankaj <chaudharypankajatt0001@gmail.com>
2021-09-24 16:50:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Landingpage from './Components/Landingpage';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Landingpage/>
+    </>
+  );
+}
+
+export default App;
